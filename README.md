331bd1394e6a6be911c0952a14868af955dd4c51
Alex Kostenko <kostenkoaleksander7@gmail.com>
2022-11-15 23:22:51 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LibPhoneNumber
+
+A description of this package.
---
+++ Sources/LibPhoneNumber/LibPhoneNumber.swift
@@ -0,0 +1,6 @@
+public struct LibPhoneNumber {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
